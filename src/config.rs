91ad2771c2d8f18959253adc812e924b056c19e4
zaira.bibi <zaira.bibi@arbisoft.com>
2025-09-02 14:23:42 +0500

    fix: Changed port to 8080

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub const BASE_URL: &str = "http://127.0.0.1:5050";
+pub const BASE_URL: &str = "http://127.0.0.1:8000";
 pub const SEARCH_ENDPOINT: &str = "search";
 pub const SEARCH_BY_AUTHOR_ENDPOINT: &str = "search_by_author";
 pub const UPLOAD_PAPERS_ENDPOINT: &str = "upload_papers";
